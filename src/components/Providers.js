2b53ac3f29d51ee5af0eae93c6f080695a9db142
OnanThairu <thairuonan@gmail.com>
2022-07-26 15:04:17 +0300

    Add assets folders, fetch data from API

--- src/components/Providers.js
+++ src/components/Providers.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Providers() {
-    return (
-        <h1>MashPoa</h1>
-    )
+  const [transporters, setTransporters] = useState([])
+  useEffect(() => {
+    fetch("http://localhost:3001/transporters")
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+        setTransporters(data)
+    })
+  },[])
+
+  return (
+      <>
+        <p>Please choose your favourite transport provider.</p>
+      {
+        transporters.map((transporter) => {
+          return (
+            <div key={ transporter.id }>
+              <h1>{ transporter.name }</h1>
+            </div>
+          )
+        })
+      }
+      </>
+  )
 }
 
 export default Providers
