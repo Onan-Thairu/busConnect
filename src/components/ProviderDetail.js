349ea98558076a54dd0aec259734685f2987a5a3
OnanThairu <thairuonan@gmail.com>
2022-07-27 23:55:05 +0300

    Display route data on a table

--- src/components/ProviderDetail.js
+++ src/components/ProviderDetail.js
@@ -1,17 +1,43 @@
 import React from "react";
-import { useParams } from "react-router-dom"
+import { useParams, Link } from "react-router-dom"
 
 function ProviderDetail({ transporters }) {
 
     const { providerName } = useParams()
     
-    const currentProvider = transporters.filter((transporter) => transporter.name === providerName)
+    let currentProvider = transporters.filter((transporter) => transporter.name === providerName)
 
-    console.log(providerName)
+    currentProvider = currentProvider[0]
+
+    const route = currentProvider.destinations.map((destination, index) => {
+
+      return (
+        <>
+          <tbody key={index}>
+            <td>{destination.from}</td>
+            <td>{destination.to}</td>
+            <td>{destination.seats}</td>
+            <td>{destination.price}</td>
+            <Link to="/BookingForm">Book</Link>
+          </tbody>
+        </>
+      )
+    })
+ 
     return (
         <div>
-            <h1>Provider Detail</h1>
-            <p>{ currentProvider[0].name }</p>
+            <h1>{ currentProvider.name }</h1>
+            <table>
+              <thead>
+                <tr>
+                  <th>FROM</th>
+                  <th>TO</th>
+                  <th>SEATS</th>
+                  <th>{`PRICE(Ksh.)`}</th>
+                </tr>
+              </thead>
+                { route }
+            </table>
         </div>
     )
 }
