e47e3dd7f77d44405ad4ed35e3f4803a93aba6c0
OnanThairu <thairuonan@gmail.com>
2022-07-28 13:54:11 +0300

    Add images and style home page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,28 @@
 import './Home.css'
 import React from "react";
+import traveler from "../images/traveler.svg"
+import bus from "../images/bus.svg"
 import { Link } from "react-router-dom"
 
 function Home() {
   return (
     <div>
       <main>
-        <p>Online Bus Booking service.</p>
-        <p>Travel hassle-free.</p>
-        <Link to="/providers" >
-          <button>BOOK BUS</button>
-        </Link>
+        <div className="hero">
+          <div className='intro'>
+            <p>Online Bus Booking service.</p>
+            <p>Travel hassle-free.</p>
+          </div>
+          <div className='images'>
+            <img src={traveler} alt="traveler" width="250px" />
+            <img src={bus} alt="bus" width="250px" />
+          </div>
+        </div>
+        <div className='bookButton'>
+          <Link to="/providers" className="bookBus" >
+            <button>BOOK BUS</button>
+          </Link>
+        </div>
       </main>
     </div>
   )
