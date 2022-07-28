2979ea3f3bb0d70cba36c9bf38aba3449e568019
OnanThairu <thairuonan@gmail.com>
2022-07-28 13:01:36 +0300

    Style navigation bar.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,15 @@
+import './Nav.css'
 import React from "react";
 import { NavLink } from "react-router-dom"
 
 function Nav() {
     return (
     <nav>
-        <h1>BusConnect</h1>
         <div>
+          <h1>BusConnect</h1>
+        </div>
+        <div className='navLinks'>
           <NavLink to="/">Home</NavLink>
-          <br />
           <NavLink to="/about">About</NavLink>
         </div>
     </nav>
