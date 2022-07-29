2308dfe1eb93801df997f0dc0af5678d02f04b9f
OnanThairu <thairuonan@gmail.com>
2022-07-29 06:29:31 +0300

    Add styling to about page.

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,11 @@
+import "./About.css"
 import React from "react";
 
 function About() {
     return (
         <div>
-          <h1>About us</h1>
-          <p>
+          <h1 className="aboutHeader">About us</h1>
+          <p className="aboutText">
             BusConnect makes bus ticket booking easy by allowing you to choose from different transport 
             providers. We are here to make your travel experience simple, efficient, convenient and hassle-free.
           </p>
