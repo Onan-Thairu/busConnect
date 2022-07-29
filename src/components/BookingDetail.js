4fcc6cd4b2c529fa6988257bde3a026d17545f8b
OnanThairu <thairuonan@gmail.com>
2022-07-29 08:41:04 +0300

    bug fix

--- src/components/BookingDetail.js
+++ src/components/BookingDetail.js
@@ -1,11 +1,27 @@
 import "./BookingDetail.css"
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function BookingDetail({ bookings }) {
+  // const [booked, setBooked] = useState([])
+
+  // useEffect(() => {
+  //   fetch("https://busconnectserver.herokuapp.com/bookings")
+  //   .then((response) => response.json())
+  //   .then((data) => setBooked(data))
+  // }, [])
+
     return (
       <>
-        <ul>
+        <p>
           <span className="ul-header">{ bookings.provider}</span>:<span className="ul-header">From</span> { bookings.from}  - <span className="ul-header">To</span>{ bookings.to} <span>({bookings.tickets} ticket(s))</span>
+        </p>
+        <ul>
+          {/* {
+            
+              
+              // <li><span className="ul-header">{ book.provider}</span>:<span className="ul-header">From</span> { book.from}  - <span className="ul-header">To</span>{ book.to} <span>({book.tickets} ticket(s))</span></li>
+            })
+          } */}
         </ul>
       </>
     )
