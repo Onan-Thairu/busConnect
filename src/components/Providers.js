3044a751d7c9648e0ee6971cd62473fdc31d1ee8
OnanThairu <thairuonan@gmail.com>
2022-07-29 05:22:32 +0300

    Style providers page

--- src/components/Providers.js
+++ src/components/Providers.js
@@ -1,4 +1,4 @@
-import "./Provider.css"
+import "./Providers.css"
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
@@ -13,18 +13,22 @@ function Providers({ transporters, setTransporters }) {
   },[])
 
   return (
-      <>
-        <p>Please choose your favourite transport provider.</p>
-      {
-        transporters.map((transporter) => {
-          return (
-            <div key={ transporter.id } className="container">
-              <Link to={`${transporter.name}`}  >{ transporter.name }</Link>
-            </div>
-          )
-        })
-      }
-      </>
+      <div>
+        <div className="choose">
+          <p>Please choose your favourite transport provider.</p>
+        </div>
+        <div className="providerContainer">
+          {
+          transporters.map((transporter) => {
+            return (
+              <div key={ transporter.id } className="providerChild">
+                <Link to={`${transporter.name}`}  >{ transporter.name }</Link>
+              </div>
+            )
+          })
+          }
+        </div>
+      </div>
   )
 }
 
